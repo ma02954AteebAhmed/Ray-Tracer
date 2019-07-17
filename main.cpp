d69be67d62013bd85b092b6aba34dfde29e038ef
ma02954AteebAhmed <35393247+ma02954AteebAhmed@users.noreply.github.com>
2019-07-18 01:45:02 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -17,33 +17,67 @@ using namespace Eigen;
 using std::cout;
 using std::endl;
 
-// limits of the scene (scene scope)
-#define MAX_WIDTH 5000
-#define MAX_HEIGHT 5000
-#define MAX_WEIGHT 5000
-
 //func decls
 void ray_cast(camera* c , sphere* s);
 void save_ppm_image(camera* c);
 
 int main()
 {
+    cout << "ma life" << endl;
 
     // starting point of image plane
-    Vector4d p1(1,1,1,1);
+    Vector3d camera_position(0,0,0);
+
+    // initializing the image plane
+    Vector3d top_left(-1,1,-1);
+    Vector3d top_right(1,1,-1);
+    Vector3d bottom_left(-1,-1,-1);
+
+    image_plane im = image_plane(top_left, top_right, bottom_left , camera_position, 2000 , 2000);
+
     // camera position
-   camera canon_5d_mk1 = camera(250 ,250, p1);
+    camera canon_5d_mk1 = camera(&im ,camera_position);
 
     // sphere
-    Vector4d c = {0,0,-3,1};
-    sphere s = sphere(c , 2);
+    Vector3d c = {1,-1,-10};
+    sphere s = sphere(c , 4);
 
-
-
+    //im.print_co_ordinates();
 
     // casting the rays
     ray_cast(&canon_5d_mk1 , &s);
     save_ppm_image(&canon_5d_mk1);
+
+
+    /*
+    // Checking collision mechanism
+    Vector3d origin (0,0,0);
+    Vector3d to_point(-0.5,0.5,-1);
+
+    line* ray = new line(origin , to_point);
+    Vector3d* collide = s.collision_detection(ray);
+
+    cout <<     collide << endl;
+
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     return 0;
 }
@@ -51,7 +85,7 @@ int main()
 void ray_cast(camera* c , sphere* s)
 {
     int counter = 0;
-    Vector4d* res;
+    Vector3d* res;
     for(int i = 0; i < c->img->p_height ; i++)
     {
         for (int j = 0; j < c->img->p_width; j++)
@@ -60,9 +94,11 @@ void ray_cast(camera* c , sphere* s)
             if (res != nullptr)
             {
                 //assigning the color
-                c->img->image_pixels[i][j].val = Vector3d(255,15,150);
-
+                c->img->image_pixels[i][j].val = Vector3i(220,150,150);
                 counter += 1;
+            }
+            else{
+                c->img->image_pixels[i][j].val = Vector3i(255,255,255);
             }
         }
     }
@@ -72,7 +108,6 @@ void ray_cast(camera* c , sphere* s)
 
 void save_ppm_image(camera* c)
 {
-
     // opening file stream
     std::ofstream myfile ;
     // opening file to write on
@@ -86,11 +121,11 @@ void save_ppm_image(camera* c)
 
 
     // WRITING PIXEL INFORMATION
-    for (int i = 0 ; i != c->img->p_height ; i++ )
+    for (int i = 0 ; i < c->img->p_height ; i++ )
     {
         for (int j = 0; j < c->img->p_width ; j++)
         {
-            for (int k = 0 ; k < 3 ; k ++)
+            for (int k = 0 ; k < 3 ; k++)
             {
                 myfile << c->img->image_pixels[i][j].val[k] << " ";
             }
@@ -98,7 +133,6 @@ void save_ppm_image(camera* c)
 
         myfile << "\n" ;
     }
-
     return;
 }
 
